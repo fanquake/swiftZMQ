af98aa9ad31e0b519eab79a6951bff19ab48dbfd
fanquake <fanquake@gmail.com>
2019-04-07 20:23:01 +0800

    add ZeroMQ version number

--- Sources/swiftZMQ/swiftZMQ.swift
+++ Sources/swiftZMQ/swiftZMQ.swift
@@ -1,5 +1,13 @@
 import zeromq
 
-struct swiftZMQ {
-    var text = "Hello, World!"
+public struct swiftZMQ {
+
+    public static var version: String {
+        var major: Int32 = 0
+        var minor: Int32 = 0
+        var patch: Int32 = 0
+        let _ = zmq_version(&major, &minor, &patch)
+        return "\(major).\(minor).\(patch)"
+    }
+
 }
