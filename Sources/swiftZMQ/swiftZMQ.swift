5b30ed476b10288c771701cd475e6369c2964dc5
fanquake <fanquake@gmail.com>
2019-04-07 20:38:55 +0800

    Add hasCapability

--- Sources/swiftZMQ/swiftZMQ.swift
+++ Sources/swiftZMQ/swiftZMQ.swift
@@ -10,4 +10,18 @@ public struct swiftZMQ {
         return "\(major).\(minor).\(patch)"
     }
 
+    public enum Capability: String {
+        case curve // CURVE security
+        case draft // DRAFT APIs
+        case ipc // ipc://
+        case gssapi // GSSAPI security
+        case norm // norm://
+        case pgm // pgm://
+        case tipc // tipc://
+    }
+
+    static public func hasCapability(_ cap: Capability) -> Bool {
+        return zmq_has(cap.rawValue) == 1
+    }
+
 }
